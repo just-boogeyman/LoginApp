ba12f5b4f0296853b31bbd932ab2c415322b3978
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-09-14 23:25:39 +0700

    setup LoginVC and WelcomeVC

---
+++ LoginApp/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Ярослав Кочкин on 14.09.2023.
+//
+
+import Foundation
+import UIKit
+
+
+final class WelcomeViewController: UIViewController {
+    
+}
